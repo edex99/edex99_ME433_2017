9a622a46aacc7d3b2721535c640a3877a1178c5f
edex99 <dexheimere@dhcp-10-105-146-98.wireless.northwestern.private>
2017-05-05 11:58:14 -0500

    Print Filtered z accelerations to screen
    Filter math still needs to be updated

---
+++ HW10/client.m
@@ -0,0 +1,45 @@
+function client(port)
+%   provides a menu for accessing PIC32 motor control functions
+%   /dev/tty.usbserial-DB00MLC9
+%
+%   client(port)
+%
+%   Input Arguments:
+%       port - the name of the com port.  This should be the same as what
+%               you use in screen or putty in quotes ' '
+%
+%   Example:
+%       client('/dev/ttyUSB0') (Linux/Mac)
+%       client('COM3') (PC)
+%
+%   For convenience, you may want to change this so that the port is hardcoded.
+   
+% Opening COM connection
+if ~isempty(instrfind)
+    fclose(instrfind);
+    delete(instrfind);
+end
+
+fprintf('Opening port %s....\n',port);
+
+% settings for opening the serial port. baud rate 230400, hardware flow control
+% wait up to 120 seconds for data before timing out
+mySerial = serial(port, 'BaudRate', 230400, 'FlowControl', 'hardware','Timeout',10); 
+% opens serial connection
+fopen(mySerial);
+% closes serial port when function exits
+clean = onCleanup(@()fclose(mySerial));                                 
+
+has_quit = false;
+% menu loop
+fprintf(mySerial,'r\n'); 
+for i=1:100
+    data(i,:) = fscanf(mySerial,'%d %d %d %d %d'); % read in data from PIC32; assume ints, in mA
+end
+
+figure;
+plot(data(:,2));
+hold on;
+plot(data(:,3));
+plot(data(:,4));
+plot(data(:,5));
